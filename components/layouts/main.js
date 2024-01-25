9ae246cb1a7ececb669650335bea22970ad075f8
Anes Hmida <33029571+AnassHmida@users.noreply.github.com>
2024-01-25 16:34:49 +0100

    fix responsive

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,12 +1,12 @@
 import NavBar from '../navbar'
-import { Flex } from '@chakra-ui/react'
-//  import Footer from '../footer'
+import { Box, Container, Flex } from '@chakra-ui/react'
+ import Footer from '../footer'
 import Head from 'next/head'
 
 
 const Main = ({ children, router }) => {
   return (
-    <Flex  >
+    <Flex >
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="description" content="MyElec est une entreprise monégasque spécialisée en travaux d'électricité générale pour les particuliers et les professionnels à Monaco et dans les Alpes Maritimes. Découvrez nos services de qualité exceptionnelle" />
@@ -31,15 +31,11 @@ const Main = ({ children, router }) => {
 
 
 
-      <Flex >
-    
-
+<Flex>
         {children}
-
-        
-      </Flex>
-
-      
+        {/* <Footer /> */}
+        </Flex>
+     
      
     </Flex>
   )
